17050cd654c2c60a83257ea8ad0738da60fe1b31
sunnyhitha <41725679+sunnyhitha@users.noreply.github.com>
2018-08-02 15:17:38 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		
+		String s;
+         Scanner sc=new Scanner(System.in);
+         s=sc.nextLine();
+		int count = 1;
+ 
+		for (int i = 0; i < s.length() - 1; i++)
+		{
+			if ((s.charAt(i) == ' ') && (s.charAt(i + 1) != ' '))
+			{
+				count++;
+ 
+			}
+		}
+ 		System.out.println(count);
+	// your code goes here
+	}
+}
